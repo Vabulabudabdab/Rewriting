85cf1050fdfee4f615201d0c48517ede49df2eec
Vabulabudabdab <Sarkisowa7@gmail.com>
2024-02-17 20:56:06 +0300

    edit some settings

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../resources/css/app.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link  href="./uploads/boosty.png">
     <title>Boosty</title>
 </head>
 <body onclick="hiddenMenu()">
@@ -18,10 +19,10 @@
             </div>
             <div class="hiddenMenu" id="dropdownMenu">
                 <div class="en">
-                    <div class="languages"><img src="../images/usaFlug.png">EN</div>
+                    <div class="languages"><img src="./uploads/brt-flag.png" style="width: 20px; height: 10px; margin-bottom: 3px;">    EN</div>
                 </div>
                 <div class="ru">
-                    <div class="languages"><img src="../images/russianFlug.png">RU</div>
+                    <div class="languages"><img src="./uploads/russianFlug.png" style="width: 20px; height: 15px; margin-bottom: 3px;"> RU</div>
                 </div>
             </div>
             <button type="button" class="buttonRegister">
